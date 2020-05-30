9be1d80ffc5c84faf172f348d240b8b9c65f9238
michaelberry0321 <46459648+michaelberry0321@users.noreply.github.com>
2020-05-30 12:35:01 -0700

    Initial Commit
    This is an initial commit. In it there is a - - homepage w/ css & html configured.
    - virtual environment called 'env'
    
    Database still needs to be set up and models created.

--- customize-my-rig/urls.py
+++ customize-my-rig/urls.py
@@ -1,4 +1,4 @@
-"""customizeMyRig URL Configuration
+"""customize-my-rig URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
