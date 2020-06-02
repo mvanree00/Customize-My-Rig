f8a9a465abb056ca58cd867f8f73373898a44bc0
mvanree00 <mvanree00@gmail.com>
2020-06-02 01:12:00 -0400

    imported part data - all except cooler and case (will hand pick those)

--- homepage/models.py
+++ homepage/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 class CPU(models.Model):
     name =  models.CharField(max_length=50)
-    cpu_fan = models.BooleanField()
+    cpu_fan = models.BooleanField(default=True)
     cores = models.PositiveSmallIntegerField()
-    platform = models.CharField(max_length=50)
+    platform = models.CharField(max_length=50,null=True)
     price = models.FloatField()
-    gaming_perf = models.PositiveSmallIntegerField()
+    gaming_perf = models.PositiveSmallIntegerField(null=True)
     tdp = models.PositiveSmallIntegerField()
-    links = models.CharField(max_length=1000) # if oos take next site
+    links = models.CharField(max_length=1000,null=True) # if oos take next site
     def __str__(self):
         return self.name
 class GPU(models.Model):
     price = models.FloatField()
-    gaming_perf = models.PositiveSmallIntegerField()
+    gaming_perf = models.PositiveSmallIntegerField(null=True)
     manufacturer = models.CharField(max_length=30)
     chipset = models.CharField(max_length=50)
-    tdp = models.PositiveSmallIntegerField()
-    links = models.CharField(max_length=1000)
+    tdp = models.PositiveSmallIntegerField(null=True)
+    links = models.CharField(max_length=1000,null=True)
     def __str__(self):
         return self.manufacturer+' '+self.chipset
 class MEM(models.Model):
@@ -26,7 +26,7 @@ class MEM(models.Model):
     cas = models.PositiveSmallIntegerField()
     modules = models.CharField(max_length=25)
     price = models.FloatField()
-    links = models.CharField(max_length=1000)
+    links = models.CharField(max_length=1000,null=True)
     def __str__(self):
         return self.name
 class STORAGE(models.Model):
@@ -35,7 +35,7 @@ class STORAGE(models.Model):
     price = models.FloatField()
     form = models.CharField(max_length=20)
     kind = models.CharField(max_length=10)
-    links = models.CharField(max_length=1000)
+    links = models.CharField(max_length=1000, null=True)
     def __str__(self):
         return self.name
 class PWR(models.Model):
@@ -43,26 +43,27 @@ class PWR(models.Model):
     wattage = models.PositiveSmallIntegerField()
     price = models.FloatField()
     rating = models.CharField(max_length=10)
-    links = models.CharField(max_length=1000)
+    links = models.CharField(max_length=1000, null=True)
     def __str__(self):
         return self.name
 class CASE(models.Model):
     name =  models.CharField(max_length=20)
     price = models.FloatField()
     size = models.CharField(max_length=10)
-    links = models.CharField(max_length=1000)
+    links = models.CharField(max_length=1000,null=True)
     def __str__(self):
         return self.name
 class MOBO(models.Model):
     name =  models.CharField(max_length=20)
     price = models.FloatField()
     chipset = models.CharField(max_length=50)
-    links = models.CharField(max_length=1000)
+    links = models.CharField(max_length=1000,null=True)
     def __str__(self):
         return self.name
 class FAN(models.Model):
     name =  models.CharField(max_length=20)
     price = models.FloatField()
-    links = models.CharField(max_length=1000)
+    links = models.CharField(max_length=1000,null=True)
+    kind = models.CharField(max_length=20)
     def __str__(self):
         return self.name
