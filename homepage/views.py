5761ae827cc46ad21c2483038ad4eea355d979c9
sadiecrawford <sadiecrawford@ufl.edu>
2020-06-13 14:30:35 -0400

    Added getBudget to views with price from slider
    - Removed unnecessary imports from views.py
    - Edited buildpc.py so the algorithm could be used
    - getBuild() from buildpc.py prints build with budget from slider

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
+from buildpc import getBuild
 
+from homepage.models import *
+from django.db.models import Max
+from links import getLink
+from buildpc import getBuild
 
 def index(request):
+    if (request.method == 'GET' and 'amount' in request.GET):
+        print('Budget:', request.GET['amount'])
+        print(getBuild(float(request.GET['amount'])))
+
     return render(request, 'homepage/index.html')
