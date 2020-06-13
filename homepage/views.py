42f32f4692da54bf74754eb28905de022227c2ab
michaelberry0321 <46459648+michaelberry0321@users.noreply.github.com>
2020-06-13 11:57:08 -0700

    print page

--- homepage/views.py
+++ homepage/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import CPU
 
 
 def index(request):
     return render(request, 'homepage/index.html')
+
+def results(request):
+    
+    data = CPU.objects.all()
+
+    cpu = {
+        "cpu_data": data
+    }
+    return render(request, 'homepage/results.html', cpu)
