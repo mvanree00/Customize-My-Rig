8f5488601f8951c812eea1b11f265a55b57c3911
Michael Rothstein <42815678+micstein@users.noreply.github.com>
2020-06-24 18:15:02 -0400

    Add info and about links

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('results', views.results, name='results'),
+    path('info.html/', views.info, name="info"),
+    path('about.html/', views.about, name="about")
 ]
