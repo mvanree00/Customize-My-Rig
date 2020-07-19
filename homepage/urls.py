d045c72379554c8cc55ce55babb83435dbeccc5d
Michael Rothstein <42815678+micstein@users.noreply.github.com>
2020-07-19 12:15:05 -0400

    Update urls.py

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('case/', views.case, name='case'),
+    path('type/', views.type, name='type'),
+    path('brand/', views.brand, name='brand'),
+    path('hardware/', views.hardware, name='hardware'),
     path('results', views.results, name='results'),
     path('info.html/', views.info, name='info'),
     path('about.html/', views.about, name='about')
