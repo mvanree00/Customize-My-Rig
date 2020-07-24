e8dd9508ee9d3e971ac7ad7b8ecd1335758424d2
mvanree00 <mvanree00@gmail.com>
2020-07-24 13:24:10 -0400

    final version of selenium price updater, can be run using 'python manage.py shell' and then 'exec(open('updatePrice.py').read())

--- updatePrice.py
+++ updatePrice.py
@@ -6,16 +6,13 @@ from time import sleep
 import os
 from homepage.models import CPU,GPU,MEM,STORAGE,PWR,CASE,MOBO,FAN
 from math import floor
-from links import getLink
-############################################################################################
-# NOTE that this takes very long to run due to spacing the requests, don't try to run this #
-############################################################################################
 redir = ['https://pcpartpicker.com/products/cpu/','https://pcpartpicker.com/products/video-card/#c=476,439,450,438,436,446,425,447,427,448,424,392,391,431,445,444&page=1&sort=chipset','https://pcpartpicker.com/products/memory/#U=4&Z=8192002,16384002,32768002&t=14&S=3000,3866&page=1&sort=price','https://pcpartpicker.com/products/internal-hard-drive/#page=1','https://pcpartpicker.com/products/power-supply/#e=6,5,4,3,2&page=1&sort=price','https://pcpartpicker.com/products/case/#t=4,3','https://pcpartpicker.com/products/motherboard/#f=2&L=4&page=1&sort=price','https://pcpartpicker.com/products/cpu-cooler/']
 def importCPU(browser):
     menu = WebDriverWait(browser,1).until(presence_of_element_located((By.CSS_SELECTOR,'#category_content')))
     sleep(.5)
     items = menu.find_elements_by_tag_name('tr')
     full = CPU.objects.all()
+    full.update(last_updated=None)
     for item in items:
         section = item.find_elements_by_tag_name('td')
         price = section[9].text
@@ -25,21 +22,14 @@ def importCPU(browser):
         temp = full.filter(links=links)
         if price == '':
             temp.update(price=None)
-            temp.update(webpage=None)
-        elif temp:
-            sleep(2)
-            output = getLink(links)
-            if output == 'OOS':
-                temp.update(price=None)
-                temp.update(webpage=None)
-            else:
-                temp.update(webpage=output[0])
-                temp.update(price=output[1])
+        else:
+            temp.update(price=price)
 def importGPU(browser):
     sleep(.5)
     pages = browser.find_element_by_css_selector('.pagination').find_elements_by_tag_name('li')
     pagesNo = int(pages[-1].text)
     full = GPU.objects.all()
+    full.update(last_updated=None)
     for i in range(1,pagesNo+1):
         menu = WebDriverWait(browser,1).until(presence_of_element_located((By.CSS_SELECTOR,'#category_content')))
         sleep(.5)
@@ -53,21 +43,13 @@ def importGPU(browser):
             temp = full.filter(links=links)
             if price == '':
                 temp.update(price=None)
-                temp.update(webpage=None)
-            elif temp:
-                sleep(2)
-                output = getLink(links)
-                if output == 'OOS':
-                    temp.update(price=None)
-                    temp.update(webpage=None)
-                else:
-                    temp.update(webpage=output[0])
-                    temp.update(price=output[1])
+            else:
+                temp.update(price=price)
         if i != pagesNo:
             browser.find_element_by_css_selector('.pagination').find_element_by_link_text(str(i+1)).click()
 def importMEM(browser):
     sleep(.5)
-    MEM.objects.all().delete()
+    MEM.objects.all().update(price=None,last_updated=None)
     for i in range(1,5):
         menu = WebDriverWait(browser,1).until(presence_of_element_located((By.CSS_SELECTOR,'#category_content')))
         sleep(.5)
@@ -86,17 +68,18 @@ def importMEM(browser):
                 color = section[6].text
                 links = section[9].find_element_by_tag_name('button').get_attribute('data-product-tag')
                 img = section[1].find_element_by_tag_name('a').find_element_by_tag_name('div').find_element_by_tag_name('div').find_element_by_tag_name('img').get_attribute('src')
-                sleep(2)
-                output = getLink(links)
-                if output != "OOS":
-                    mem = MEM(name=name,speed=speed,cas=cas,modules=modules,price=output[1],links=links,img=img,color=color,webpage=output[0])
+                temp = MEM.objects.all().filter(links=links)
+                if temp:
+                    temp.update(price=price)
+                else:
+                    mem = MEM(name=name,speed=speed,cas=cas,modules=modules,price=price,links=links,img=img,color=color)
                     mem.save()
         if i != 4:
             browser.find_element_by_css_selector('.pagination').find_element_by_link_text(str(i+1)).click()
 def importSTORAGE(browser):
     browser.get('https://pcpartpicker.com/products/internal-hard-drive/#page=1')
     sleep(.5)
-    STORAGE.objects.all().delete()
+    STORAGE.objects.all().update(price=None,last_updated=None)
     for i in range(1,5):
         menu = WebDriverWait(browser,1).until(presence_of_element_located((By.CSS_SELECTOR,'#category_content')))
         sleep(.5)
@@ -117,17 +100,18 @@ def importSTORAGE(browser):
                 price = price[1:]
                 links = section[9].find_element_by_tag_name('button').get_attribute('data-product-tag')
                 img = section[1].find_element_by_tag_name('a').find_element_by_tag_name('div').find_element_by_tag_name('div').find_element_by_tag_name('img').get_attribute('src')
-                sleep(2)
-                output = getLink(links)
-                if output != "OOS":
-                    storage = STORAGE(name=name,capacity=capacity,price=output[1],form=form,kind=kind,links=links,img=img,webpage=output[0])
+                temp = STORAGE.objects.all().filter(links=links)
+                if temp:
+                    temp.update(price=price)
+                else:
+                    storage = STORAGE(name=name,capacity=capacity,price=price,form=form,kind=kind,links=links,img=img)
                     storage.save()
         if i != 4:
             browser.find_element_by_css_selector('.pagination').find_element_by_link_text(str(i+1)).click()
 def importPWR(browser):
     browser.get('https://pcpartpicker.com/products/power-supply/#e=6,5,4,3,2&page=1&sort=price')
     sleep(.5)
-    PWR.objects.all().delete()
+    PWR.objects.all().update(price=None,last_updated=None)
     for i in range(1,3):
         menu = WebDriverWait(browser,1).until(presence_of_element_located((By.CSS_SELECTOR,'#category_content')))
         sleep(.5)
@@ -144,10 +128,11 @@ def importPWR(browser):
                 price = price[1:]
                 img = section[1].find_element_by_tag_name('a').find_element_by_tag_name('div').find_element_by_tag_name('div').find_element_by_tag_name('img').get_attribute('src')
                 links = section[8].find_element_by_tag_name('button').get_attribute('data-product-tag')
-                sleep(2)
-                output = getLink(links)
-                if output != "OOS":
-                    pwr = PWR(name=name,wattage=wattage,price=output[1],rating=rating,links=links,img=img,webpage=output[0])
+                temp = PWR.objects.all().filter(links=links)
+                if temp:
+                    temp.update(price=price)
+                else:
+                    pwr = PWR(name=name,wattage=wattage,price=price,rating=rating,links=links,img=img)
                     pwr.save()
         if i != 2:
             browser.find_element_by_css_selector('.pagination').find_element_by_link_text(str(i+1)).click()
@@ -166,22 +151,14 @@ def importCASE(browser):
         temp = full.filter(links=links)
         if price == '':
             temp.update(price=None)
-            temp.update(webpage=None)
-        elif temp:
-            sleep(2)
-            output = getLink(links)
-            if output == 'OOS':
-                temp.update(price=None)
-                temp.update(webpage=None)
-            else:
-                temp.update(webpage=output[0])
-                temp.update(price=output[1])
+        else:
+            temp.update(price=price)
 def importMOBO(browser):
     browser.get('https://pcpartpicker.com/products/motherboard/#f=2&L=4&page=1&sort=price')
     menu = WebDriverWait(browser,1).until(presence_of_element_located((By.CSS_SELECTOR,'#category_content')))
     sleep(.5)
     items = menu.find_elements_by_tag_name('tr')
-    MOBO.objects.all().delete()
+    MOBO.objects.all().update(price=None,last_updated=None)
     for item in items:
         section = item.find_elements_by_tag_name('td')
         if section[8].text[0]=='$': # price has id we can search later
@@ -192,10 +169,11 @@ def importMOBO(browser):
             price = price[1:]
             links = section[8].find_element_by_tag_name('button').get_attribute('data-product-tag')
             img = section[1].find_element_by_tag_name('a').find_element_by_tag_name('div').find_element_by_tag_name('div').find_element_by_tag_name('img').get_attribute('src')
-            sleep(2)
-            output = getLink(links)
-            if output != "OOS":
-                mobo = MOBO(name=name,price=output[1],chipset=chipset,links=links,img=img,webpage=output[0])
+            temp = MOBO.objects.all().filter(links=links)
+            if temp:
+                temp.update(price=price)
+            else:
+                mobo = MOBO(name=name,price=price,chipset=chipset,links=links,img=img)
                 mobo.save()
 def importFAN(browser):
     browser.get('https://pcpartpicker.com/products/cpu-cooler/')
@@ -212,16 +190,8 @@ def importFAN(browser):
         temp = full.filter(links=links)
         if price == '':
             temp.update(price=None)
-            temp.update(webpage=None)
-        elif temp:
-            sleep(2)
-            output = getLink(links)
-            if output == 'OOS':
-                temp.update(price=None)
-                temp.update(webpage=None)
-            else:
-                temp.update(webpage=output[0])
-                temp.update(price=output[1])
+        else:
+            temp.update(price=price)
 path = os.getcwd()+'\\geckodriver.exe'
 browser=webdriver.Firefox(executable_path=path)
 browser.get(redir[0])
