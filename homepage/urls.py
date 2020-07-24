f62cf0cddd692e83130257e22dfd2d890377506c
Hannah McEachern <hexart@ufl.edu>
2020-07-24 11:50:54 -0400

    Updates to Retrieve Past Builds
    Added a model for builds that stores all of the links for the parts.
    - Edited views.py for the results and hardware views to use the new results handler

--- homepage/urls.py
+++ homepage/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('type/', views.type, name='type'),
     path('brand/', views.brand, name='brand'),
     path('hardware/', views.hardware, name='hardware'),
+    path('results/<int:build_ID>', views.results, name='pastResults'),
     path('results', views.results, name='results'),
     path('info.html/', views.info, name='info'),
     path('about.html/', views.about, name='about')
